c46e80bd318345122b41f5fde7fd2b05bfeabfcb
EssienAbasiama <essienabasiama11@gmail.com>
2024-01-24 01:49:43 +0100

    MainBody Developmemt

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import SideBar from "./components/Sidebar/SideBar";
 import Header from "./components/Header/Header";
+import MainBody from "./components/MainBody/MainBody";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <SideBar />
         <div className="body">
           <Header />
+          <MainBody />
         </div>
       </div>
     </>
